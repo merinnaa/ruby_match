c2be4300bbb764716c94acf6e97bdb8689cfaa8a
luwam <lwamyohannes2018@gmail.com>
2023-11-19 15:54:43 -0700

    match.rb created

---
+++ match.rb
@@ -0,0 +1,7 @@
+# class match - will initialize the variables and condtional loop method
+# method initialize - initialize the name the score and the numbers to be added
+# method play - holds the conditional loop - if player one is correct then it dispalys player2 response and change the score
+# class game - 
+# initioalize the payer instance
+# method - start te game, handle game turns anounce the score
+# class user- displayer the input and output 
